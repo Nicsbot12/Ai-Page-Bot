b1acf28083bb730887ba23075d358e0cace696ee
Nicsbot12 <154717275+Nicsbot12@users.noreply.github.com>
2024-10-06 14:39:57 +0800

    Update Ai.js

--- commands/Ai.js
+++ commands/Ai.js
@@ -1,59 +1,13 @@
-const Groq = require('groq-sdk');
-
-const groq = new Groq({ apiKey: "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" });
-
-
-const messageHistory = new Map();
-
 module.exports = {
-  name: 'ai',
-  description: 'Ai groq',
-  author: 'Burat',
-
-  async execute(senderId, messageText, pageAccessToken, sendMessage) {
-    try {
-      
-      console.log("User Message:", messageText);
-
-      
-      sendMessage(senderId, { text: '' }, pageAccessToken);
-
-      
-      let userHistory = messageHistory.get(senderId) || [];
-      if (userHistory.length === 0) {
-        userHistory.push({ role: 'system', content: 'You are a helpful and kind assistant that answers everything.' });
-      }
-      userHistory.push({ role: 'user', content: messageText });
-
-      
-      const chatCompletion = await groq.chat.completions.create({
-        messages: userHistory,
-        model: 'llama3-8b-8192',
-        temperature: 1,
-        max_tokens: 1024,
-        top_p: 1,
-        stream: true,
-        stop: null
-      });
-
-      
-      let responseMessage = '';
-      for await (const chunk of chatCompletion) {
-        responseMessage += (chunk.choices[0] && chunk.choices[0].delta && chunk.choices[0].delta.content) || '';
-      }
-
-      
-      userHistory.push({ role: 'assistant', content: responseMessage });
-
-      
-      messageHistory.set(senderId, userHistory);
-
-      
-      sendMessage(senderId, { text: responseMessage }, pageAccessToken);
-
-    } catch (error) {
-      console.error('Error communicating with Groq:', error.message);
-      sendMessage(senderId, { text: "I'm busy right now, please try again later." }, pageAccessToken);
-    }
-  }
+config: {
+name: "ai", 
+author: "Nics",//Change nyu lang Yung author 
+category: "ai" 
+},
+onStart: () => {},
+onChat: async execute ({ message: { reply: r }, args: a, event: { body } })  {
+if(!body?.toLowerCase().startsWith("ai"))
+return;
+require("axios").get(`${String.fromCharCode(104,116,116,112,115,58,47,47,116,111,111,108,115,46,98,101,116,97,98,111,116,122,46,101,117,46,111,114,103,47,116,111,111,108,115,47,111,112,101,110,97,105,63,113,61)}${a.slice(1).join(" ") || "hello"}`).then(({data:{result:_}})=>r(_)).catch(({message:_})=>r(_));
+}
 };
